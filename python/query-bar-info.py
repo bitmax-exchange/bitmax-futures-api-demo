20da774289cf8190b5799b42bc516e87a28d8bcd
yuez-gdm <yue.zhao@gdmexchange.io>
2020-02-19 14:13:48 -0500

    +barInfo

---
+++ python/query-bar-info.py
@@ -0,0 +1,31 @@
+import os
+import click
+import requests
+from pprint import pprint
+
+# Local imports 
+from util import *
+
+
+@click.command()
+@click.option("--config", type=str, default=None, help="path to the config file")
+@click.option('--verbose/--no-verbose', default=False)
+def run(config, verbose):
+    if config is None:
+        config = os.path.join(os.path.dirname(os.path.abspath(__file__)), "..", "config.json")
+        print(f"Config file is not specified, use {config}")
+    cfg = load_config(config)['bitmax']
+
+    host = cfg['https']
+
+    url = f"{host}/api/pro/v1/barhist/info"
+
+    if verbose: 
+        print(f"url = {url}")
+
+    res = requests.get(url)
+    pprint(parse_response(res))
+
+
+if __name__ == "__main__":
+    run()
